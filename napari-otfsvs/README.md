e32918cf19fc8ab46438ba2c4d009b8ddcd97a90
Yuchen Zhang <zhangyuchen000@outlook.com>
2024-06-01 20:26:41 -0500

    Add On-The-Fly svs viewer

---
+++ napari-otfsvs/README.md
@@ -0,0 +1,61 @@
+# napari-otfsvs
+
+[![License BSD-3](https://img.shields.io/pypi/l/napari-otfsvs.svg?color=green)](https://github.com/githubuser/napari-otfsvs/raw/main/LICENSE)
+[![PyPI](https://img.shields.io/pypi/v/napari-otfsvs.svg?color=green)](https://pypi.org/project/napari-otfsvs)
+[![Python Version](https://img.shields.io/pypi/pyversions/napari-otfsvs.svg?color=green)](https://python.org)
+[![tests](https://github.com/githubuser/napari-otfsvs/workflows/tests/badge.svg)](https://github.com/githubuser/napari-otfsvs/actions)
+[![codecov](https://codecov.io/gh/githubuser/napari-otfsvs/branch/main/graph/badge.svg)](https://codecov.io/gh/githubuser/napari-otfsvs)
+[![napari hub](https://img.shields.io/endpoint?url=https://api.napari-hub.org/shields/napari-otfsvs)](https://napari-hub.org/plugins/napari-otfsvs)
+
+On-The-Fly SVS Reader
+
+----------------------------------
+
+This [napari] plugin was generated with [Cookiecutter] using [@napari]'s [cookiecutter-napari-plugin] template.
+
+<!--
+Don't miss the full getting started guide to set up your new package:
+https://github.com/napari/cookiecutter-napari-plugin#getting-started
+
+and review the napari docs for plugin developers:
+https://napari.org/stable/plugins/index.html
+-->
+
+## Installation
+
+You can install `napari-otfsvs` via [pip]:
+
+    pip install napari-otfsvs
+
+
+
+
+## Contributing
+
+Contributions are very welcome. Tests can be run with [tox], please ensure
+the coverage at least stays the same before you submit a pull request.
+
+## License
+
+Distributed under the terms of the [BSD-3] license,
+"napari-otfsvs" is free and open source software
+
+## Issues
+
+If you encounter any problems, please [file an issue] along with a detailed description.
+
+[napari]: https://github.com/napari/napari
+[Cookiecutter]: https://github.com/audreyr/cookiecutter
+[@napari]: https://github.com/napari
+[MIT]: http://opensource.org/licenses/MIT
+[BSD-3]: http://opensource.org/licenses/BSD-3-Clause
+[GNU GPL v3.0]: http://www.gnu.org/licenses/gpl-3.0.txt
+[GNU LGPL v3.0]: http://www.gnu.org/licenses/lgpl-3.0.txt
+[Apache Software License 2.0]: http://www.apache.org/licenses/LICENSE-2.0
+[Mozilla Public License 2.0]: https://www.mozilla.org/media/MPL/2.0/index.txt
+[cookiecutter-napari-plugin]: https://github.com/napari/cookiecutter-napari-plugin
+
+[napari]: https://github.com/napari/napari
+[tox]: https://tox.readthedocs.io/en/latest/
+[pip]: https://pypi.org/project/pip/
+[PyPI]: https://pypi.org/
---
+++ napari-otfsvs/src/napari_otfsvs/_reader.py
@@ -0,0 +1,126 @@
+"""
+This module is an example of a barebones numpy reader plugin for napari.
+
+It implements the Reader specification, but your plugin may choose to
+implement multiple readers or even other plugin contributions. see:
+https://napari.org/stable/plugins/guides.html?#readers
+"""
+
+import numpy as np
+import openslide
+import dask
+import dask.array as da
+from openslide import deepzoom as dz
+
+
+def napari_get_reader(path):
+    """A basic implementation of a Reader contribution.
+
+    Parameters
+    ----------
+    path : str or list of str
+        Path to file, or list of paths.
+
+    Returns
+    -------
+    function or None
+        If the path is a recognized format, return a function that accepts the
+        same path or list of paths, and returns a list of layer data tuples.
+    """
+    if isinstance(path, list):
+        # A path list does not make sense for this plugin
+        return None
+
+    # otherwise we return the *function* that can read ``path``.
+    if path.endswith(".svs"):
+        return reader_function
+
+    # otherwise we return the *function* that can read ``path``.
+    return None
+
+
+def reader_function(path):
+    """Take a path or list of paths and return a list of LayerData tuples.
+
+    Readers are expected to return data as a list of tuples, where each tuple
+    is (data, [add_kwargs, [layer_type]]), "add_kwargs" and "layer_type" are
+    both optional.
+
+    Parameters
+    ----------
+    path : str or list of str
+        Path to file, or list of paths.
+
+    Returns
+    -------
+    layer_data : list of tuples
+        A list of LayerData tuples where each tuple in the list contains
+        (data, metadata, layer_type), where data is a numpy array, metadata is
+        a dict of keyword arguments for the corresponding viewer.add_* method
+        in napari, and layer_type is a lower-case string naming the type of
+        layer. Both "meta", and "layer_type" are optional. napari will
+        default to layer_type=="image" if not provided
+    """
+    tile_size = 512
+    overlap = 0
+
+    slide = openslide.open_slide(path)
+    gen = dz.DeepZoomGenerator(
+        slide, tile_size=tile_size, overlap=overlap, limit_bounds=True
+    )
+    num_levels = gen.level_count
+
+    @dask.delayed(pure=True)
+    def get_tile(level, column, row):
+        tile = gen.get_tile(level, (column, row))
+        return np.array(tile).transpose((1, 0, 2))
+
+    myPyramid = []
+    # Loop through each level and read the image data
+    for level in reversed(range(num_levels)):
+        # Get dimensions for the current level
+        level_dimensions = gen.level_dimensions[level]
+
+        sample_tile_shape = get_tile(level, 0, 0).shape.compute()
+        n_tiles_x, n_tiles_y = gen.level_tiles[level]
+        # Remove last tile because it has a custom size.
+        if n_tiles_x <= 1 or n_tiles_y <= 1:
+            print(
+                f"Ignoring Level {level} with dimensions: {level_dimensions}"
+            )
+            continue
+        else:
+            print(f"Reading Level {level} with dimensions: {level_dimensions}")
+        rows = range(n_tiles_y - 1)
+        cols = range(n_tiles_x - 1)
+
+        arr = da.concatenate(
+            [
+                da.concatenate(
+                    [
+                        da.from_delayed(
+                            get_tile(level, col, row),
+                            sample_tile_shape,
+                            np.uint8,
+                        )
+                        for row in rows
+                    ],
+                    allow_unknown_chunksizes=False,
+                    axis=1,
+                )
+                for col in cols
+            ],
+            allow_unknown_chunksizes=False,
+        )
+
+        # Append the numpy array to the list
+        myPyramid.append(arr)
+
+    # optional kwargs for the corresponding viewer.add_* method
+    add_kwargs = {
+        "multiscale": True,
+        "contrast_limits": [0, 255],
+    }
+    layer_type = "image"  # optional, default is "image"
+
+    return [(myPyramid, add_kwargs, layer_type)]
